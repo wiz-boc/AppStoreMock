cfe64fe278d7bd238e3efca506d9fdb5315122fb
wiz-boc <wiz_kidz@outlook.com>
2024-05-06 21:13:35 -0500

    -- Created Detail view -- fetch Detail json

---
+++ AppStoreMock/AppDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  AppDetailView.swift
+//  AppStoreMock
+//
+//  Created by wizz on 5/6/24.
+//
+
+import SwiftUI
+
+struct AppDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppDetailView()
+}
